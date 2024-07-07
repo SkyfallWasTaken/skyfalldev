a446fdfc3f24a8d614d2d3def7f4cff70db3fed6
SkyfallWasTaken <mahadkalam1@proton.me>
2024-07-07 21:27:54 +0100

    Blog post styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,31 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-body': theme('colors.pink[800]'),
+            '--tw-prose-headings': theme('colors.text'),
+            '--tw-prose-links': theme('colors.text'),
+            '--tw-prose-hr': theme('colors.pink[300]'),
+            '--tw-prose-quotes': theme('colors.text'),
+            '--tw-prose-kbd': theme("colors.text"),
+
+            '--tw-prose-quote-borders': theme('colors.pink[300]'),
+            '--tw-prose-captions': theme('colors.pink[700]'),
+            '--tw-prose-code': theme('colors.text'),
+            '--tw-prose-pre-code': theme('colors.text'),
+            '--tw-prose-pre-bg': theme('colors.pink[900]'),
+          },
+        },
+      }),
+    }
   },
   plugins: [
     require("@catppuccin/tailwindcss")({
       defaultFlavour: "macchiato",
     }),
+    require("@tailwindcss/typography"),
   ],
 };
