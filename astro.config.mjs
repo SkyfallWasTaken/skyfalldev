8cdaa9e69a965cd9f3ed4ea9fb1c7827cb609f32
SkyfallWasTaken <mahadkalam1@proton.me>
2024-07-14 13:40:05 +0100

    Add sitemap.xml, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  // FIXME: don't hardcode this
   site: "https://skyfall.dev",
   integrations: [mdx(), sitemap(), tailwind(), svelte()],
   /* output: "hybrid", // or 'server'
