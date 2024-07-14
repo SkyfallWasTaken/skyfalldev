1bf9af4349c1122b5c26d6983f5610b8ae34da19
SkyfallWasTaken <mahadkalam1@proton.me>
2024-07-14 14:14:14 +0100

    Add Sentry, use env var for site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,21 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import sentry from '@sentry/astro';
+import "dotenv/config"
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  // FIXME: don't hardcode this
-  site: "https://skyfall.dev",
-  integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  site: process.env.SITE,
+  integrations: [mdx(), sitemap(), tailwind(), svelte(), sentry({
+    dsn: process.env.SENTRY_DSN,
+    sourceMapsUploadOptions: {
+      project: process.env.SENTRY_PROJECT_SLUG,
+      authToken: process.env.SENTRY_AUTH_TOKEN
+    }
+  })],
   /* output: "hybrid", // or 'server'
   experimental: {
     actions: true,
